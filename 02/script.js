f039d4a2b25b3b5ca810a554c6690a7ea108be4c
Quang Pham <quangpham@ucmerced.edu>
2025-05-27 23:00:16 -0700

    improve project 2

--- 02/script.js
+++ 02/script.js
@@ -1,38 +1,31 @@
-const MAX_STEP = 3;
-let step = 0;
-
-// Tailwind step classes
-// w-[calc((100%-var(--circle))*0/3)]
-// w-[calc((100%-var(--circle))*1/3)]
-// w-[calc((100%-var(--circle))*2/3)]
-// w-[calc((100%-var(--circle))*3/3)]
-const stepClass = (step) => `w-[calc((100%-var(--circle))*${step}/3)]`;
-
-// Config
-const SHADE_ACTIVE = "blue-400";
-const SHADE_DEACTIVE = "gray-200";
-const DATA_NEXT = {
-  delta: 1,
-  addShade: SHADE_ACTIVE,
-  remShade: SHADE_DEACTIVE,
-};
-const DATA_PREV = {
-  delta: -1,
-  addShade: SHADE_DEACTIVE,
-  remShade: SHADE_ACTIVE,
-};
-
 // Action buttons
 const btnPrev = document.querySelector('[data-action="prev"]');
 const btnNext = document.querySelector('[data-action="next"]');
 const progressBar = document.querySelector('[data-target="progress-bar"]');
 const progressStep = document.querySelectorAll('[data-target="progress-step"]');
 
+const MAX_STEP = progressStep.length - 1;
+let step = 0;
+
+// Config
+const SHADE_ACTIVE = "blue-400";
+const SHADE_DEACTIVE = "gray-200";
+const CFG_NEXT = {
+  delta: 1,
+  addShade: SHADE_ACTIVE,
+  remShade: SHADE_DEACTIVE,
+};
+const CFG_PREV = {
+  delta: -1,
+  addShade: SHADE_DEACTIVE,
+  remShade: SHADE_ACTIVE,
+};
+
 // Event handler
 function handler() {
   // Set up config
   const isNext = this === btnNext;
-  let { delta, addShade, remShade } = isNext ? DATA_NEXT : DATA_PREV;
+  let { delta, addShade, remShade } = isNext ? CFG_NEXT : CFG_PREV;
   const stepBefore = step;
   step += delta;
 
@@ -41,13 +34,13 @@ function handler() {
   btnNext.disabled = step === MAX_STEP;
 
   // Update progress bar
-  progressBar.classList.remove(stepClass(stepBefore));
-  progressBar.classList.add(stepClass(step));
+  progressBar.style.width = `${(step / MAX_STEP) * 100}%`;
 
   // Update progress step
   const progUpdateStep = isNext ? step : stepBefore;
-  progressStep[progUpdateStep].classList.add(`border-${addShade}`);
-  progressStep[progUpdateStep].classList.remove(`border-${remShade}`);
+  const progCircle = progressStep[progUpdateStep];
+  progCircle.classList.add(`border-${addShade}`);
+  progCircle.classList.remove(`border-${remShade}`);
 }
 
 // Add click events
